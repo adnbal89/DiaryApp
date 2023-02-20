29758441d9efbbf115577f297d0a83774033a28c
Adnan BAL <adnbal89@hotmail.com>
2023-02-20 21:31:11 +0300

    - NavGraph implemented.

--- app/src/main/java/com/aceofhigh/diaryapp/MainActivity.kt
+++ app/src/main/java/com/aceofhigh/diaryapp/MainActivity.kt
@@ -3,14 +3,13 @@ package com.aceofhigh.diaryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.aceofhigh.diaryapp.navigation.Screen
+import com.aceofhigh.diaryapp.navigation.SetupNavGraph
 import com.aceofhigh.diaryapp.ui.theme.DiaryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,13 +18,11 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             DiaryAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                val navController = rememberNavController()
+                SetupNavGraph(
+                    startDestination = Screen.Authentication.route,
+                    navController = navController
+                )
             }
         }
     }
--- app/src/main/java/com/aceofhigh/diaryapp/util/Constants.kt
+++ app/src/main/java/com/aceofhigh/diaryapp/util/Constants.kt
@@ -3,4 +3,6 @@ package com.aceofhigh.diaryapp.util
 object Constants {
     const val APP_ID = "mydiaryapp-lhyvx"
     const val CLIENT_ID = "4488714987-bg5esdsdlorr4oqne0fnpr5a383bktkn.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
 }
